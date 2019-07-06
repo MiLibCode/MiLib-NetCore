eb20c2534cef4fbcaacc5314e202f8a0a197b318
Moses Ibiwoye <mosesparadise@live.com>
2019-07-06 12:03:38 +0100

    First Public Release

---
+++ src/MiLibNetCore.Core/System/Collections/Generic/MiEnumerableExtensions.cs
@@ -0,0 +1,125 @@
+﻿using System.Linq;
+
+namespace System.Collections.Generic
+{    
+    /// <summary> 
+    /// Extension methods for <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    public static class MiEnumerableExtensions
+    {
+        /// <summary>
+        /// Concatenates the members of a constructed <see cref="IEnumerable{T}"/> collection of type System.String, using the specified separator between each member.
+        /// This is a shortcut for string.Join(...)
+        /// </summary>
+        /// <param name="source">A collection that contains the strings to concatenate.</param>
+        /// <param name="separator">The string to use as a separator. separator is included in the returned string only if values has more than one element.</param>
+        /// <returns>A string that consists of the members of values delimited by the separator string. If values has no members, the method returns System.String.Empty.</returns>
+        public static string JoinAsString(this IEnumerable<string> source, string separator)
+        {
+            return string.Join(separator, source);
+        }
+
+        /// <summary>
+        /// Concatenates the members of a collection, using the specified separator between each member.
+        /// This is a shortcut for string.Join(...)
+        /// </summary>
+        /// <param name="source">A collection that contains the objects to concatenate.</param>
+        /// <param name="separator">The string to use as a separator. separator is included in the returned string only if values has more than one element.</param>
+        /// <typeparam name="T">The type of the members of values.</typeparam>
+        /// <returns>A string that consists of the members of values delimited by the separator string. If values has no members, the method returns System.String.Empty.</returns>
+        public static string JoinAsString<T>(this IEnumerable<T> source, string separator)
+        {
+            return string.Join(separator, source);
+        }
+
+        /// <summary>
+        /// Filters a <see cref="IEnumerable{T}"/> by given predicate if given condition is true.
+        /// </summary>
+        /// <param name="source">Enumerable to apply filtering</param>
+        /// <param name="condition">A boolean value</param>
+        /// <param name="predicate">Predicate to filter the enumerable</param>
+        /// <returns>Filtered or not filtered enumerable based on <paramref name="condition"/></returns>
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, bool condition, Func<T, bool> predicate)
+        {
+            return condition
+                ? source.Where(predicate)
+                : source;
+        }
+
+        /// <summary>
+        /// Filters a <see cref="IEnumerable{T}"/> by given predicate if given condition is true.
+        /// </summary>
+        /// <param name="source">Enumerable to apply filtering</param>
+        /// <param name="condition">A boolean value</param>
+        /// <param name="predicate">Predicate to filter the enumerable</param>
+        /// <returns>Filtered or not filtered enumerable based on <paramref name="condition"/></returns>
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source, bool condition, Func<T, int, bool> predicate)
+        {
+            return condition
+                ? source.Where(predicate)
+                : source;
+        }
+
+        /// <summary>
+        /// Enumerates for each in this collection.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="list">The list to act on.</param>
+        /// <param name="action">The action.</param>
+        public static void Each<T>(this IEnumerable<T> list, Action<T> action)
+        {
+            foreach (var item in list)
+            {
+                action(item);
+            }
+        }
+
+        /// <summary>
+        /// Enumerates for each in this collection.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="list">The list to act on.</param>
+        /// <param name="action">The action.</param>
+        public static void Each<T>(this IEnumerable list, Action<T> action)
+        {
+            foreach (var item in list)
+            {
+                action((T)item);
+            }
+        }
+
+        /// <summary>
+        ///     Enumerates for each in this collection.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="action">The action.</param>
+        /// <returns>An enumerator that allows foreach to be used to process for each in this collection.</returns>
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> @this, Action<T> action)
+        {
+            T[] array = @this.ToArray();
+            foreach (T t in array)
+            {
+                action(t);
+            }
+            return array;
+        }
+
+        /// <summary>Enumerates for each in this collection.</summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="action">The action.</param>
+        /// <returns>An enumerator that allows foreach to be used to process for each in this collection.</returns>
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> @this, Action<T, int> action)
+        {
+            T[] array = @this.ToArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                action(array[i], i);
+            }
+
+            return array;
+        }
+    }
+}
