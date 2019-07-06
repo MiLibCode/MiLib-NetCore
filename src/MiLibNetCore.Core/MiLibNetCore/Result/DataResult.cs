95564a3141afaee02366cb73d3cb59077a07b3d0
Moses Ibiwoye <mosesparadise@live.com>
2019-07-06 20:34:12 +0100

    Added Generic Result Implementation

---
+++ src/MiLibNetCore.Core/MiLibNetCore/Result/DataResult.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace MiLibNetCore.Result
+{
+    public class DataResult<T> : Result, IDataResult<T>
+    {
+        public DataResult(T data, bool success, string message) : base(success, message)
+        {
+            Data = data;
+        }
+
+        public DataResult(T data, bool success) : base(success)
+        {
+            Data = data;
+        }
+
+        public T Data { get; }
+
+        public new Task<IDataResult<T>> ToTask()
+        {
+            return Task.FromResult(this as IDataResult<T>);
+        }
+    }
+}
---
+++ src/MiLibNetCore.Core/MiLibNetCore/Result/SuccessResult.cs
@@ -0,0 +1,13 @@
+﻿namespace MiLibNetCore.Result
+{
+    public sealed class SuccessResult : Result
+    {
+        public SuccessResult(string message) : base(true, message)
+        {
+        }
+
+        public SuccessResult() : base(true)
+        {
+        }
+    }
+}
