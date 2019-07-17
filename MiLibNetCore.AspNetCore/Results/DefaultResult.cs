07997308c54feb39205f84873dcb54a7039b5020
Moses Ibiwoye <mosesparadise@live.com>
2019-07-18 00:56:44 +0100

    Added NameValue Custom Class

---
+++ MiLibNetCore.AspNetCore/Results/DefaultResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiLibNetCore.AspNetCore.Results
+{
+   //public class DefaultResult : IActionResult
+   // {
+   // }
+}
